07827df544374ebe608e9aa1504745798bc55e58
devThilosurin <thilosurin@gmail.com>
2023-11-27 19:38:30 +0700

    update: fix unit test

--- src/test/kotlin/com/test/randomuser/RandomUserApplicationTests.kt
+++ src/test/kotlin/com/test/randomuser/RandomUserApplicationTests.kt
@@ -1,16 +1,4 @@
 package com.test.randomuser
 
-import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class RandomUserApplicationTests {
-    @Autowired
-    private lateinit var webTestClient: WebTestClient
-
-    @Test
-    fun `should return list of user`() {
-    }
-}
+class RandomUserApplicationTests{}
--- src/test/kotlin/com/test/randomuser/controller/UserControllerTest.kt
+++ src/test/kotlin/com/test/randomuser/controller/UserControllerTest.kt
@@ -1,23 +1,45 @@
 package com.test.randomuser.controller
 
 import com.test.randomuser.entity.User
+import com.test.randomuser.service.UserService
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpHeaders
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.WebTestClient.ListBodySpec
+import reactor.core.publisher.Mono
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class UserControllerTest {
-    @Autowired
-    private lateinit var webTestClient: WebTestClient
+
+@WebFluxTest(UserController::class)
+class UserControllerTest(
+    @Autowired private val webTestClient: WebTestClient,
+) {
+    @MockBean
+    private lateinit var userService: UserService
 
     @Test
-    fun `should return list of user`() {
+    fun `should return list of users`() {
+        val responseBody = mutableListOf(
+            User(
+                firstName = "آیناز",
+                lastName = "کریمی",
+                gender = "female",
+                email = "aynz.khrymy@example.com"
+            )
+        )
+
+        given(userService.getUserWithSeed("foobar"))
+            .willReturn(Mono.fromCallable { responseBody })
+
         webTestClient.get().uri("/v1/users/foobar")
-            .header(HttpHeaders.ACCEPT, "application/json")
             .exchange()
             .expectStatus().isOk()
             .expectBodyList(User::class.java)
+            .consumeWith<ListBodySpec<User>> { user ->
+                Assertions.assertEquals(responseBody, user.responseBody)
+            }
     }
 }
